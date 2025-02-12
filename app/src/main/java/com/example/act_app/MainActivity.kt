12389379668973fbf95d497452eecca34ca20b38
Steven Essam <steven.essam.edwar@gmail.com>
2025-02-12 15:27:17 +0100

    collect data from website polygon

--- app/src/main/java/com/example/act_app/MainActivity.kt
+++ app/src/main/java/com/example/act_app/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.act_app
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.webkit.JavascriptInterface
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,12 +22,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val webView = findViewById<WebView>(R.id.webView)
+
         webView.webViewClient = WebViewClient()  // Makes the links open within the WebView
 
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true  // Enable JavaScript if needed
         webSettings.domStorageEnabled = true  // Enable DOM Storage if needed
 
-        webView.loadUrl("https://act.gitlabpages.inria.fr/website/")  // Your website URL
+        // Ajouter l'interface JavaScript à la WebView
+        webView.addJavascriptInterface(WebAppInterface(this), "Android")
+
+        webView.loadUrl("http://192.168.237.32/")  // Your website URL
+    }
+}
+
+class WebAppInterface(private val context: Context) {
+
+    @JavascriptInterface
+    fun sendDataToAndroid(data: String) {
+        // Traitez les données reçues ici
+        Log.d("WebAppInterface", "Data received from WebView: $data")
     }
 }
